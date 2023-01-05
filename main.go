9df76d32366312224d4de771899d42fdb4ee21fa
Nicolasios <1498913522@qq.com>
2023-01-05 10:49:46 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	_ "BookStore/routers"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.Debug = true
+	orm.RegisterDataBase("default", "mysql", "root:728827@tcp(127.0.0.1:3306)/beego?charset=utf8")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
